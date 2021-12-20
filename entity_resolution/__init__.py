aad21a3d1c2f5e49305b16593959b1f7f0623501
Swa <swa@orbifold.net>
2021-12-20 14:21:07 +0100

    The .nojekyll thing.

--- entity_resolution/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = """Francois Vanderseypen"""
-__email__ = 'swa@orbifold.net'
-__version__ = '1.3.0'
-
-from .data import *
-from .visualization import draw
-from .inference import infer_identity_edges, infer_jaccard_identity, infer_identities
-from .solutions import *
